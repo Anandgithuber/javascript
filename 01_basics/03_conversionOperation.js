96f5347db03eb52477cfe1588f5157d092ef14b3
Anand Kumar <93638537+Anandgithuber@users.noreply.github.com>
2023-10-16 08:27:01 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+//let score = "33abc" //string
+// let score = null or undefine 
+//let score = "anand"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+//for string to  number conversion
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber) //number
+//console.log(valueInNumber) //NaN ,0 , NaN, Nan
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0;
+
+
+//number to boolean
+let isLOggedIn = 1
+let bolleanIsLoggedIn = Boolean(isLOggedIn)
+//console.log(bolleanIsLoggedIn)
+
+//1 => true
+//0 => false
+//"" => false
+//"anand" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
